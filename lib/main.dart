87c99028a913fb8f343d4f8e683b9b03e4d3f14a
runcycle <justinbartholomew@gmail.com>
2019-05-28 16:52:23 -0400

    Added app bar and body, starting to build widget that will update state with random answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue[300],
+          appBar: AppBar(
+            title: Center(
+              child: Text('Ask Me Anything ...'),
+            ),
+            backgroundColor: Colors.blue[200],
+          ),
+          body: EightBallPage(),
+        ),
       ),
     );
+
+class EightBallPage extends StatefulWidget {
+  @override
+  _EightBallPageState createState() => _EightBallPageState();
+}
+
+class _EightBallPageState extends State<EightBallPage> {
+  int eightBallAnswer = 1;
+
+  void changeAnswer() {
+    eightballAnswer = Random().nextInt(5) + 1;
+  }
+
+  Widget build(BuildContext context) {
+    
+  }
+}
